d8546cc730e2a80b3a35bf6c5134a5abe4363346
Joe Schmitz <josephschmitz2018@u.northwestern.edu>
2019-04-07 13:17:00 -0500

    getting rid of the color shit

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 // ==============================================================================
 
 var express = require("express");
+var path = require('path');
 
 // ==============================================================================
 // EXPRESS CONFIGURATION
@@ -19,7 +20,7 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, './app/public')));
 // ================================================================================
 // ROUTER
 // The below points our server to a series of "route" files.
